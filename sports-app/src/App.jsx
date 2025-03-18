5cd3d0fb74b4d7eddb72714efe86932dbb923221
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2025-03-18 17:04:57 +1100

    vite set up

---
+++ sports-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className=''>
+      <div className='h-14 flex justify-center '>
+        <h1 className='font-bold'>Sports App</h1>
+      </div>
+      <div className='h-px w-full bg-red-800'></div>
+      <div className='font-bold'>Hello world</div>
+    </div>
+  )
+}
+
+export default App
